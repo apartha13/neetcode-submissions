f0b99035bad10480177638255cbee68658dd8d39
Ajay Partha <134064626+apartha13@users.noreply.github.com>
2026-07-27 14:41:35 -0500

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = {course: [] for course in range(numCourses)}
+
+        for course, pre in prerequisites:
+            courses[course].append(pre)
+        
+        for course in range(numCourses):
+            stack = [(course, set())]
+            while stack:
+                curr, visited = stack.pop()
+                if curr in visited:
+                    return False
+                visited.add(curr)
+                for pre in courses[curr]:
+                    stack.append((pre, visited.copy()))
+            courses[course] = []
+        
+        return True
