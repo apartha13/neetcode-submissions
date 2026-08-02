c6ce3af286f5b05cacaaebc7fdd7a1f02432a669
Ajay Partha <134064626+apartha13@users.noreply.github.com>
2026-08-02 17:36:19 -0500

    Add: climbing-stairs - submission-17

---
+++ Data Structures & Algorithms/climbing-stairs/submission-17.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if not n:
+            return 0
+        elif n < 2:
+            return 1
+            
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
